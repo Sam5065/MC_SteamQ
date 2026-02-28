8d30525fc580cbba1017fd4cc88b7edd6297582a
Sam5065 <sam.hilland1@icloud.com>
2026-02-28 18:12:54 +0000

    Misc starter

---
+++ src/main/java/com/kelpshot/steamstatus/SteamStatusMod.java
@@ -0,0 +1,10 @@
+package src.main.java.com.kelpshot.steamstatus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SteamStatusMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Steam Status Mod Loaded!");
+    }
+}
